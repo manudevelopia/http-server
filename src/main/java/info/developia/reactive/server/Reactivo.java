386c9d5ce6de23a6fa97b5b5b3f6980f5c7b8a43
manudevelopia <manu@developia.info>
2022-12-25 11:48:21 +0100

    support providing port

--- src/main/java/info/developia/reactive/server/Reactivo.java
+++ src/main/java/info/developia/reactive/server/Reactivo.java
@@ -6,14 +6,20 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.logging.Logger;
 
 public class Reactivo {
+    private static final Logger log = Logger.getLogger(Reactivo.class.getName());
+
     private final HttpServer httpServer;
+    private final int port;
     private boolean virtualThreads = true;
     private int threadsInPool = 200;
+    private String status;
 
-    private Reactivo(HttpServer httpServer) {
+    private Reactivo(HttpServer httpServer, int port) {
         this.httpServer = httpServer;
+        this.port = port;
     }
 
     public static Reactivo init() throws IOException {
@@ -22,12 +28,13 @@ public class Reactivo {
 
     public static Reactivo initOn(int port) throws IOException {
         var httpServer = HttpServer.create(new InetSocketAddress(port), 0);
-        return new Reactivo(httpServer);
+        return new Reactivo(httpServer, port);
     }
 
     public void start() {
         httpServer.setExecutor(getExecutor(virtualThreads));
         httpServer.start();
+        log.info("Server started on %s".formatted(port));
     }
 
     public Reactivo addRoute(String path, RequestHandler handler) {
