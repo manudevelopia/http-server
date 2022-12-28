358220f89302c0fc8b207cdb13928190b647cb52
Manuel Garcia <Manuel.Garcia@zooplus.com>
2022-12-28 11:49:15 +0100

    improve scope

--- lib/src/main/java/info/developia/reactive/server/RequestHandler.java
+++ lib/src/main/java/info/developia/reactive/server/RequestHandler.java
@@ -9,15 +9,15 @@ import static info.developia.reactive.server.Method.GET;
 public class RequestHandler {
     private final List<RouteHandler> routeHandlers = new ArrayList<>();
 
-    public void get(BiConsumer<Request, Response> handler) {
+    protected void get(BiConsumer<Request, Response> handler) {
         routeHandlers.add(new RouteHandler(GET, handler));
     }
 
-    public void get(String path, BiConsumer<Request, Response> handler) {
+    protected void get(String path, BiConsumer<Request, Response> handler) {
         routeHandlers.add(new RouteHandler(GET, path, handler));
     }
 
-    public List<RouteHandler> routeHandlers() {
+    List<RouteHandler> routeHandlers() {
         return routeHandlers;
     }
 }
