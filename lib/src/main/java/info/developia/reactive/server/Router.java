e6c7fe25a3f7149c60dd5db4f1fc74d4c19383da
Manuel Garcia <Manuel.Garcia@zooplus.com>
2023-01-18 16:29:30 +0100

    support static uris

--- lib/src/main/java/info/developia/reactive/server/Router.java
+++ lib/src/main/java/info/developia/reactive/server/Router.java
@@ -6,34 +6,17 @@ import java.util.function.BiConsumer;
 
 public class Router {
     private final Map<String, BiConsumer<Request, Response>> staticRoutes = new HashMap<>();
-    private final Map<String, BiConsumer<Request, Response>> parametricRoutes = new HashMap<>();
 
     public void map(String basePath, RequestHandler handler) {
         handler.routeHandlers().forEach((routeHandler) -> {
-            if (routeHandler.hasPathVariables()) {
-                parametricRoutes.put(routeHandler.method() + basePath + routeHandler.pathPattern(), routeHandler.handler());
-            } else {
-                staticRoutes.put(routeHandler.method() + basePath + routeHandler.pathPattern(), routeHandler.handler());
-            }
+            staticRoutes.put(routeHandler.method() + basePath + routeHandler.pathPattern(), routeHandler.handler());
         });
     }
 
     public BiConsumer<Request, Response> getHandler(String handlerKey) {
-        if(staticRoutes.containsKey(handlerKey)){
-            return staticRoutes.get(handlerKey);
-        }
-
-        checkParametric(handlerKey);
-
-        return staticRoutes.computeIfAbsent(handlerKey, (e) -> (req, res) -> {
+        return staticRoutes.getOrDefault(handlerKey, (req, res) -> {
             res.status(404);
             res.body("not found!!!");
         });
-    }
-
-    private void checkParametric(String handlerKey) {
-        String[] requestUrlParts = handlerKey.split("/");
-        parametricRoutes.entrySet().stream()
-                .filter(route-> route.getKey().startsWith(requestUrlParts[0]))
     }
 }
